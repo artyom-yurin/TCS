8cb09325110f5656fadfb68dd81b9e6713f708ed
artyom-yurin <artem_yrin@mail.ru>
2018-09-10 22:30:00 +0300

    Error 2 was processed

--- assignment1/main.cpp
+++ assignment1/main.cpp
@@ -3,37 +3,18 @@
 #include <string>
 #include <utility>
 #include <vector>
+#include <set>
+#include <map>
 
 using namespace std;
 
-struct Transaction{
+struct Transaction {
     string firstState;
     string nameTransaction;
-    string finalTransaction;
+    string secondState;
 };
 
-vector<Transaction> getTransactions(vector<string> trans)
-{
-    vector<Transaction> result;
-    while(!trans.empty())
-    {
-        string transactionString = trans.back();
-        trans.pop_back();
-        Transaction newTrans;
-        size_t startPosition = 0;
-        size_t finishPosition = transactionString.find('>', startPosition);
-        newTrans.firstState = transactionString.substr(startPosition, finishPosition);
-        startPosition = finishPosition + 1;
-        finishPosition = transactionString.find('>', startPosition);
-        newTrans.nameTransaction = transactionString.substr(startPosition, finishPosition - startPosition);
-        startPosition = finishPosition + 1;
-        newTrans.finalTransaction = transactionString.substr(startPosition);
-        result.push_back(newTrans);
-    }
-    return result;
-}
-
-vector<string> getValues(string states, const string &prefix) {
+vector<string> getValues(const string &states, const string &prefix) {
     string temp = states.substr(0, states.size());
     vector<string> result;
     size_t startPosition = temp.find(prefix, 0);
@@ -47,8 +28,7 @@ vector<string> getValues(string states, const string &prefix) {
             }
             if (finishPosition != string::npos) {
                 string value = temp.substr(startPosition, finishPosition - startPosition);
-                if (!value.empty())
-                {
+                if (!value.empty()) {
                     result.push_back(value);
                 }
                 startPosition = finishPosition + 1;
@@ -58,6 +38,100 @@ vector<string> getValues(string states, const string &prefix) {
         }
     }
     return result;
+}
+
+vector<Transaction> getTransactions(const string &transString) {
+    vector<string> trans = getValues(transString, "trans={");
+    vector<Transaction> result;
+    while (!trans.empty()) {
+        string transactionString = trans.back();
+        trans.pop_back();
+        Transaction newTrans;
+        size_t startPosition = 0;
+        size_t finishPosition = transactionString.find('>', startPosition);
+        newTrans.firstState = transactionString.substr(startPosition, finishPosition);
+        startPosition = finishPosition + 1;
+        finishPosition = transactionString.find('>', startPosition);
+        newTrans.nameTransaction = transactionString.substr(startPosition, finishPosition - startPosition);
+        startPosition = finishPosition + 1;
+        newTrans.secondState = transactionString.substr(startPosition);
+        result.push_back(newTrans);
+    }
+    return result;
+}
+
+int checkState(const string & stateString, vector<string> &states, ostream &output) {
+
+    int index = -1;
+    bool e1 = true;
+    for (int i = 0; i < states.size(); i++) {
+        if (!(states[i].compare(stateString))) {
+            e1 = false;
+            index = i;
+        }
+    }
+
+    if (e1) {
+        output << "Error:\nE1: A state " + stateString +" is not in set of states\n";
+    }
+    return index;
+}
+
+int checkInitialState(vector<string> &initState, vector<string> &states, ostream &output) {
+    if (initState.empty()) {
+        output << "Error:\nE4: Initial state is not defined";
+        return -1;
+    }
+
+    return checkState(initState.back(), states, output);
+}
+
+void dfs (int v, bool * used, int** g, unsigned long size) {
+    used[v] = true;
+    for (int i = 0; i < size; i++)
+        if(g[v][i] == 1)
+        {
+            if (!used[i])
+                dfs (i, used, g, size);
+        }
+}
+
+bool checkJoint(int initStateId, vector<string> states, vector<Transaction> transactions)
+{
+    int** g = new int*[states.size()];
+    for(int i = 0; i < states.size(); ++i)
+        g[i] = new int[states.size()];
+
+    bool* used = new bool [states.size()];
+
+    map<string, int> stateId;
+
+    for(int i = 0; i < states.size();i++)
+    {
+        used[i] = false;
+        stateId.insert(pair<string, int>(states[i], i));
+        for(int j = 0; j < states.size(); j++)
+        {
+            g[i][j] = 0;
+        }
+    }
+
+    for(Transaction & trans: transactions)
+    {
+        g[stateId[trans.firstState]][stateId[trans.secondState]] = 1;
+        g[stateId[trans.secondState]][stateId[trans.firstState]] = 1;
+    }
+
+    dfs(initStateId, used, g, states.size());
+
+    for(int i =0; i < states.size(); i++)
+    {
+        if(!used[i])
+        {
+            return false;
+        }
+    }
+    return true;
 }
 
 int main() {
@@ -81,54 +155,42 @@ int main() {
     std::getline(input, initStateString);
     vector<string> initState = getValues(initStateString, "init.st={");
 
-    if(initState.empty())
-    {
-        output << "Error:\nE4: Initial state is not defined";
+    int initStateId = checkInitialState(initState, states, output);
+
+    if (initStateId == -1) {
         return 0;
     }
-
-    bool e1 = true;
-    for(const string &state: states)
-    {
-        if(!(state.compare(initState.back())))
-        {
-            e1 = false;
-        }
-    }
-
-    if(e1)
-    {
-        output << "Error:\nE1: A state s is not in set of states\n";
-        return 0;
-    }
-
     std::string finalStateString;
     std::getline(input, finalStateString);
     vector<string> finalStates = getValues(finalStateString, "fin.st={");
 
     std::string transString;
     std::getline(input, transString);
-    vector<Transaction> transactions = getTransactions(getValues(transString, "trans={"));
+    vector<Transaction> transactions = getTransactions(transString);
 
-    bool e3 = false;
     for (Transaction trans: transactions) {
-        if(!e3)
+        if(checkState(trans.firstState, states, output) != -1 && checkState(trans.secondState, states, output) != -1)
         {
             bool exist = false;
-            for(const string &alph: alpha)
-            {
-                if(trans.nameTransaction.compare(alph) == 0)
-                {
+            for (const string &alph: alpha) {
+                if (trans.nameTransaction.compare(alph) == 0) {
                     exist = true;
                 }
             }
-            e3 = !exist;
+
+            if(!exist)
+            {
+                output << "Error:\nE3: A transition a is not represented in the alphabet\n";
+                return 0;
+            }
+        } else {
+            return 0;
         }
     }
 
-    if(e3)
+    if(!checkJoint(initStateId, states, transactions))
     {
-        output << "Error:\nE3: A transition a is not represented in the alphabet\n";
+        output << "Error:\nE2: Some states are disjoint\n";
         return 0;
     }
 
